b29aced58403f7e0ee1caea22374238cf5285fb2
Yogesh Gaikwad <yogeshmgaikwad@outlook.com>
2021-01-17 20:27:32 +0530

    Updated code to work for all scenarios

--- symmetric-diff.js
+++ symmetric-diff.js
@@ -1,22 +1,16 @@
 function sym() {
-    let result = [];    
-    for (let i = 0; i < arguments.length - 1; i++) {
-      let common = [];
-      let A = ( i== 0 ) ? arguments[i] : result;
+    let result = [];
+    for ( let i = 0; i < arguments.length - 1; i++ ) {
+      let A = ( i == 0 ) ? arguments[i] : result;
       let B = arguments[i+1];
-      let temp = A;
-      A = A.length > B.length ? A : B;
-      B = temp.length > B.length ? B : temp;
-  
       //console.log("A: " + A);
       //console.log("B: " + B);
+
       let arr = [];
       for (let j = 0 ; j < A.length; j++) {
         let matchFound = false;
         for (let k = 0 ; k < B.length; k++) {
-          if(A[j] == B[k]) {
-            B.splice(k, 1);
-            common.push(A[j]);
+          if (A[j] == B[k]) {
             //console.log(B);
             matchFound = true;
             break;
@@ -27,19 +21,29 @@ function sym() {
           arr.push(A[j]);
         }
       }
-
-      //console.log("arr: " + arr);
-      //console.log("B: " + B);    
-      sortAndRemoveDuplicates(B);
-      sortAndRemoveDuplicates(common);
+  
       sortAndRemoveDuplicates(arr);
-      arr = [ ...arr, ...B];
-      result = arr.sort();
-      
+
+      for (let j = 0 ; j < B.length; j++) {
+        let matchFound = false;
+        for (let k = 0 ; k < A.length; k++) {
+          if (B[j] == A[k]) {
+            //console.log(B);
+            matchFound = true;
+            break;
+          }
+        }
+  
+        if (!matchFound) {
+          arr.push(B[j]);
+        }
+      }
+  
+      sortAndRemoveDuplicates(arr);
+
+      result = arr;
       console.log(result);
     }
-    
-    console.log(result);
     
     return result;
   }
@@ -53,6 +57,7 @@ function sym() {
     }
   }
   
+   
   //sym([1, 2, 3], [5, 2, 1, 4]);
   //sym([1, 2, 3, 3], [5, 2, 1, 4]);
   //sym([1, 2, 5], [2, 3, 5], [3, 4, 5]);
